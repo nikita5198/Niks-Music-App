732e76303db2526f24b7def3a53f0375ad56d660
nikita5198 <nikitap5198@gmail.com>
2023-03-29 09:33:05 +0530

    set token in local storage

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Favorite from "./Favorite";
 import Feed from "./Feed";
@@ -11,19 +11,33 @@ import SpotifyButton from "../SpotifyButton";
 
 
 function Home() {
+  const [token,setToken]=useState("");
+  useEffect(()=>{
+    const token=window.localStorage.getItem("token"); 
+    const hash=window.location.hash;
+    window.location.hash="";
+    if(!token && hash){
+      const _token=hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("token",_token);
+      setToken(_token);
+    }else{
+      setToken(token);
+    }
+  },[])
+
   return (
+    !token?<SpotifyButton/>:
     <div>
       <BrowserRouter>
       <div className="main-body">
-        <SpotifyButton/>
-        {/* <Sidebar/> */}
-      {/* <Routes>
+        <Sidebar/>
+      <Routes>
           <Route path="/" element={<Library />} />
           <Route path="/feed" element={<Feed />} />
           <Route path="/trending" element={<Trending />} />
           <Route path="/player" element={<Player />} />
           <Route path="/favorite" element={<Favorite />} />
-        </Routes>   */}
+        </Routes>  
       </div>
       </BrowserRouter>
     </div>
